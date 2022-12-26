63d3c37c044281e1d621a8693df045c2b5e3bc56
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-26 17:08:49 -0500

    Database settings

--- proshare_auth/settings.py
+++ proshare_auth/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'proshare_auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prosharebackend',
+        'USER': 'syedhamzaali',
+        'PASSWORD': 'Dnu99n393',
+        'HOST': 'localhost',
     }
 }
 
