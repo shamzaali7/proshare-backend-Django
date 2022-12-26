6fd00dd0d146839a67ce8d349d378ccd003751b1
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-26 17:19:48 -0500

    spelling error serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import get_user_model
 
 
-class UserCreateSerializers(UserCreateSerializer):
+class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = get_user_model()
         fields = ('id', 'email', 'name', 'password')
